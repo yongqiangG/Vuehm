354838837638d8049eec2370012c248e4cf99e6c
Johnny <johnny.gor10@gmail.com>
2020-02-21 17:28:27 +0800

    职位信息完善

--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -2,10 +2,10 @@
     <div>
         <el-tabs v-model="activeName" type="card">
             <el-tab-pane label="部门管理" name="first"><DepMana></DepMana></el-tab-pane>
-            <el-tab-pane label="职位管理" name="second"><EcMana></EcMana></el-tab-pane>
+            <el-tab-pane label="职位管理" name="second"><PosMana></PosMana></el-tab-pane>
             <el-tab-pane label="职称管理" name="third"><JobLevel></JobLevel></el-tab-pane>
-            <el-tab-pane label="奖惩规则" name="fourth"><PermissMana></PermissMana></el-tab-pane>
-            <el-tab-pane label="权限组" name="fourth2"><PosMana></PosMana></el-tab-pane>
+            <el-tab-pane label="奖惩规则" name="fourth"><EcMana></EcMana></el-tab-pane>
+            <el-tab-pane label="权限组" name="fourth2"><PermissMana></PermissMana></el-tab-pane>
         </el-tabs>
     </div>
 </template>

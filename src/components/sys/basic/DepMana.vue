6f6739d3323a0752e430ccaa18aaa3251aad334b
Johnny <johnny.gor10@gmail.com>
2020-02-27 17:21:07 +0800

    操作员

--- src/components/sys/basic/DepMana.vue
+++ src/components/sys/basic/DepMana.vue
@@ -8,10 +8,56 @@
                     v-model="depSearch">
             </el-input>
         </div>
-        <div>
-            <el-tree :data="depTree" :props="defaultProps" show-checkbox node-key="id" :key="index"
-                     ref="depTree">
+        <div class="depMain">
+            <el-tree :data="depTree" :props="defaultProps" node-key="id" ref="depTree"
+                     :expand-on-click-node="false"
+                     :filter-node-method="filterNode">
+                <span class="custom-tree-node" slot-scope="{ node, data }"
+                      style="display: flex;justify-content: space-between;width:100%;">
+        <span>{{ data.name }}</span>
+        <span class="btnClass">
+          <el-button style="padding:3px;"
+                     type="primary"
+                     size="mini"
+                     @click="showAddDep(data)">
+            添加部门
+          </el-button>
+          <el-button style="padding:3px;"
+                     type="danger"
+                     size="mini"
+                     @click="() => delDep(data)">
+            删除部门
+          </el-button>
+        </span>
+      </span>
             </el-tree>
+        </div>
+        <div>
+            <el-dialog
+                    title="添加部门"
+                    :visible.sync="dialogVisible"
+                    width="30%">
+                <table>
+                    <tr>
+                        <td>
+                            <el-tag type="success">上级部门</el-tag>
+                        </td>
+                        <td>{{parentName}}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <el-tag type="success">部门信息</el-tag>
+                        </td>
+                        <td>
+                            <el-input v-model="dep.name"></el-input>
+                        </td>
+                    </tr>
+                </table>
+                <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="doAddDep">确 定</el-button>
+  </span>
+            </el-dialog>
         </div>
 
     </div>
@@ -20,19 +66,131 @@
 <script>
     export default {
         name: "DepMana",
-        data(){
-            return{
-                depSearch:'',
-                depTree:[],
-                defaultProps:{
-                    children: 'id',
+        watch: {
+            depSearch(val) {
+                this.$refs.depTree.filter(val);
+            }
+        },
+        data() {
+            return {
+                depSearch: '',
+                depTree: [],
+                defaultProps: {
+                    children: 'children',
                     label: 'name'
+                },
+                dialogVisible: false,
+                dep: {
+                    name: '',
+                    parentId: -1
+                },
+                parentName: ''
+            }
+        },
+        mounted() {
+            this.initDepTree();
+        },
+        methods: {
+            //动态删除部门
+            delDepArr(p,depTree, id) {
+                for (let i = 0; i < depTree.length; i++) {
+                    let dep = depTree[i]
+                    if (dep.id==id){
+                        depTree.splice(i,1)
+                        if(depTree.length==0){
+                            //将父节点parent标志设置为false
+                            p.parent=false
+                        }
+                    return;
+                    }else{
+                        this.delDepArr(dep,dep.children,id)
+                    }
+                        }
+            },
+            //删除部门
+            delDep(data) {
+                if (data.parent) {
+                    this.$message.error("删除失败,该部门存在子部门")
+                } else {
+                    this.$confirm('此操作将永久删除[' + data.name + '], 是否继续?', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }).then(() => {
+                        this.deleteRequest('/system/basic/dep/' + data.id).then(resp => {
+                            if(resp){
+                                this.delDepArr(null,this.depTree,data.id)
+                            }
+
+                        }).catch(() => {
+                            this.$message({
+                                type: 'info',
+                                message: '已取消删除'
+                            });
+                        });
+                    })
                 }
+            },
+            initDep() {
+                this.dep = {
+                    name: '',
+                    parentId: -1
+                }
+                this.parentName = '';
+            },
+            //动态添加部门
+            addDepTree(depTree, dep) {
+                for (let i = 0; i < depTree.length; i++) {
+                    let d = depTree[i]
+                    if (d.id == dep.parentId) {
+                        d.children = d.children.concat(dep);
+                        console.log(d.children)
+                        if (d.children.length > 0) {
+                            d.parent = true;
+                        }
+                        return;
+                    } else {
+                        this.addDepTree(d.children, dep);
+                    }
+                }
+            },
+            //添加部门对话框
+            showAddDep(data) {
+                this.parentName = data.name
+                this.dep.parentId = data.id
+                this.dialogVisible = true;
+            },
+            //添加部门
+            doAddDep() {
+                this.postRequest('/system/basic/dep/', this.dep).then(resp => {
+                    if (resp) {
+                        this.dialogVisible = false;
+                        //动态添加部门
+                        this.addDepTree(this.depTree, resp.obj);
+                        this.initDep();
+                    }
+                })
+            },
+            //搜索过滤部门树
+            filterNode(value, data) {
+                if (!value) return true;
+                return data.name.indexOf(value) !== -1;
+            },
+            //初始化部门树
+            initDepTree() {
+                let rootNode = -1
+                this.getRequest('/system/basic/dep/p/' + rootNode).then(resp => {
+                    this.depTree = resp;
+                })
             }
         }
     }
 </script>
 
 <style scoped>
+    .depMain {
+        width: 500px;
+        margin-top: 5px;
+    }
 
 </style>
--- src/views/sys/SysHr.vue
+++ src/views/sys/SysHr.vue
@@ -1,13 +1,149 @@
 <template>
-    <div>操作员管理</div>
+    <div>
+        <div class="searchInput">
+            <el-input
+                    size="small"
+                    style="width:400px;"
+                    placeholder="输入用户姓名,回车搜索"
+                    @keydown.enter.native="searchBtn"
+                    prefix-icon="el-icon-search"
+                    v-model="keyword">
+            </el-input>
+            <el-button type="primary" size="small" style="margin-left: 8px" @click="searchBtn" icon="el-icon-search">
+                搜索
+            </el-button>
+        </div>
+        <div class="hrMain">
+            <el-card v-for="(hr,index) in hrs" :key="index" class="hrCard">
+                <div slot="header" class="clearfix">
+                    <span>{{hr.name}}</span>
+                    <el-button style="float: right; padding: 3px 0;color: red" type="text"
+                               icon="el-icon-delete"></el-button>
+                </div>
+                <div class="userImg">
+                    <img :src="hr.userface" :alt="hr.name" :title="hr.name" class="user-img">
+                </div>
+                <div class="userInfo">
+                    <div>用户名: {{hr.name}}</div>
+                    <div>手机号码: {{hr.phone}}</div>
+                    <div>电话号码: {{hr.telephone}}</div>
+                    <div>地址: {{hr.address}}</div>
+                    <div>用户状态:
+                        <el-switch
+                                v-model="hr.enabled"
+                                @change="enabledChange(hr)"
+                                active-color="#13ce66"
+                                inactive-color="#ff4949"
+                                active-text="启用"
+                                inactive-text="禁用">
+                        </el-switch>
+                    </div>
+                    <div>
+                        用户角色:<el-tag v-for="(role,indexj) in hr.roles" :key="indexj" style="margin-left: 3px;" type="success">{{role.nameZh}}</el-tag>
+                        <el-popover
+                                placement="right"
+                                title="角色列表"
+                                @hide="hidePop(hr)"
+                                @show="showPop(hr)"
+                                width="200"
+                                trigger="click">
+                            <el-select v-model="selectedRole" multiple placeholder="请选择">
+                                <el-option
+                                        v-for="(r,index) in allRoles"
+                                        :key="index"
+                                        :label="r.nameZh"
+                                        :value="r.name">
+                                </el-option>
+                            </el-select>
+                            <el-button slot="reference" icon="el-icon-more" type="text"></el-button>
+                        </el-popover>
+                    </div>
+                    <div>备注: {{hr.remark}}</div>
+                </div>
+            </el-card>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "SysHr"
+        name: "SysHr",
+        data() {
+            return {
+                keyword: '',
+                hrs: [],
+                allRoles:[],
+                selectedRole:''
+            }
+        },
+        mounted() {
+            this.initHrs()
+        },
+        methods: {
+            searchBtn() {
+
+            },
+            enabledChange(hr){
+
+            },
+            initHrs() {
+                this.getRequest('/system/hr/').then(resp => {
+                    if (resp) {
+                        this.hrs = resp;
+                    }
+                })
+            },
+            initRoles(){
+                this.getRequest('/system/basic/per/roles').then(resp=>{
+                    if(resp){
+                        this.allRoles=resp;
+                    }
+                })
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
+    .userInfo div{
+        font-size: 14px;
+        color: #409eff;
+    }
 
+    .userInfo {
+        margin-top: 20px;
+    }
+
+    .searchInput {
+        display: flex;
+        justify-content: center;
+    }
+
+    .hrMain {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        margin-top: 5px;
+    }
+
+    .hrCard {
+        width: 350px;
+        margin-bottom: 20px;
+    }
+
+    .userImg {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+
+    .user-img {
+        width: 72px;
+        height: 72px;
+        border-radius: 72px;
+    }
+
+    .userInfo {
+
+    }
 </style>
